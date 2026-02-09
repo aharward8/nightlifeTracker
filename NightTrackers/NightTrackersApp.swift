c2fed002db1aef26f27d76bfe03a0c0256c772c1
aharward8 <adamharward21@gmail.com>
2026-02-09 18:08:22 -0500

    Initialization of files.

---
+++ NightTrackers/NightTrackersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightTrackersApp.swift
+//  NightTrackers
+//
+//  Created by Adam Harward on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct NightTrackersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
